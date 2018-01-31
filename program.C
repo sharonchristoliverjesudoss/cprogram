12a53b7fddc1c9008c288fb50ffbdd30173f7692
sharonchristoliverjesudoss <35994913+sharonchristoliverjesudoss@users.noreply.github.com>
2018-01-31 16:44:35 +0530

    program.C

---
+++ program.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+clrscr();
+int a;
+if (a>0);
+{
+printf("positive number");
+}
+else if (a<0);
+printf("negitive")
+else
+printf("zero");
+return 0 ;
+getch ();
+}
