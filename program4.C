e5d04eda4a8a8caea723752c77e448e7c8a42196
sharonchristoliverjesudoss <35994913+sharonchristoliverjesudoss@users.noreply.github.com>
2018-02-02 14:01:05 +0530

    program4.C

---
+++ program4.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+char a;
+clrscr();
+scanf("%c",a);
+printf("enter the letter");
+if ((a>='a')&&(a<='z'))
+printf("alphabet");
+else
+printf("not a alphabet")
+getch();
+return 0;
+}
