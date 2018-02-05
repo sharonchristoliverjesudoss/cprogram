49b7f15892da71a057f7cd5faf17e1388f213c5f
sharonchristoliverjesudoss <35994913+sharonchristoliverjesudoss@users.noreply.github.com>
2018-02-05 15:57:50 +0530

    program7.C

---
+++ program7.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,i;
+clrscr ();
+printf("enter the number");
+scanf("%c",&a);
+for (i=1;i=a;i++)
+{ 
+printf ("hello");
+}
+getch();
+return 0;
+}
