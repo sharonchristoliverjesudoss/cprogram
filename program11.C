89c612ab855d854da742d8613d6ee6af9717a3bb
sharonchristoliverjesudoss <35994913+sharonchristoliverjesudoss@users.noreply.github.com>
2018-02-05 16:39:23 +0530

    program11.C

---
+++ program11.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main ()
+{
+int a,b,c;
+clrscr();
+scanf("%d",&a);
+printf("enter the number",a);
+scanf("%d",&b);
+printf("enter the power",b);
+c=a^b
+printf("the answer",c);
+getch();
+return 0;
+}
