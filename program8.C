1d8d4fc34f8b0c84a41c8ed53f4f2295ba97aecc
sharonchristoliverjesudoss <35994913+sharonchristoliverjesudoss@users.noreply.github.com>
2018-02-05 16:10:14 +0530

    program8.C

---
+++ program8.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main ()
+{
+int i,a,b=0;
+clrscr();
+printf("enter the number");
+scanf("%d,&a");
+for (i=0;i<=a,i++)
+{ b=b+i
+}printf("the value is %d",a)
+getch();
+return 0;
+}
