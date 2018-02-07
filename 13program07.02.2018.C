c244ed7099df01d982d232418670820133ca8caf
sharonchristoliverjesudoss <35994913+sharonchristoliverjesudoss@users.noreply.github.com>
2018-02-07 15:51:53 +0530

    13program07.02.2018.C

---
+++ 13program07.02.2018.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main ();
+{
+int a;
+clrscr();
+printf("enter the number");
+scanf("%d,&a");
+if (a%a=0 && a%1=0)
+printf("yes");
+else 
+printf("no");
+getch();
+return 0;
+}
