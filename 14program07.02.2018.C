13e6018a2b9710ee10de661e4c4e87e0e0afc8ce
sharonchristoliverjesudoss <35994913+sharonchristoliverjesudoss@users.noreply.github.com>
2018-02-07 16:22:34 +0530

    14program07.02.2018.C

---
+++ 14program07.02.2018.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b,c,n;
+clrscr();
+
+printf("print value entered in m and c":\n");
+for(n=0;n<=0;n++)
+{
+if(n%2==1)
+printf("%d",n);
+}
+getch();
+return 0;
+}
+
