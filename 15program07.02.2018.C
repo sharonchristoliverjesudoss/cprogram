240a55993f2389d15841f6ddca92198e786fd3df
sharonchristoliverjesudoss <35994913+sharonchristoliverjesudoss@users.noreply.github.com>
2018-02-07 16:29:25 +0530

    15program07.02.2018.C

---
+++ 15program07.02.2018.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	 int a,b,c;
+	printf("enter the first number");
+	scanf("%d",&a);
+	printf("enter the second number");
+	scanf("%d",&b);
+	for(c=a;c<=b;c++)
+	{
+		if(c%2==0)
+		printf("%d",c);
+	}
+  getch();
+  return 0;
+}
