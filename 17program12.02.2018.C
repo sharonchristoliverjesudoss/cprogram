97208f466556580a2ffb0ed674d3a194b37b21e5
sharonchristoliverjesudoss <35994913+sharonchristoliverjesudoss@users.noreply.github.com>
2018-02-12 15:52:45 +0530

    17program12.02.2018.C

---
+++ 17program12.02.2018.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b,c,a1,b1,c1;
+printf("enter the number");
+scanf("%d%d%d,&a,&b,&c");
+if 
+(a1=a*a*a
+b1=b*b*b
+c1=c*c*c
+a1+b1+c1=a b c )
+printf("armstrong");
+else 
+printf("not");
+getch();
+return 0;
+ }
