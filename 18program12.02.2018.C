43558436689dd8e73f8ff4da17901f09b6ed9dd5
sharonchristoliverjesudoss <35994913+sharonchristoliverjesudoss@users.noreply.github.com>
2018-02-12 16:09:26 +0530

    18program12.02.2018.C

---
+++ 18program12.02.2018.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b,c,d,e,f,g,i;
+clrscr();
+printf("enter  the lower number");
+scanf("%d",&a);
+printf("entet the higher value");
+scanf("%d",&b);
+for (i=a;i<=b;i++)
+g=(c*c*c)+(d*d*d)+(e*e*e)
+{
+i=(g==f)
+}
+printf("armstrong number");
+getch();
+return 0;
+}
