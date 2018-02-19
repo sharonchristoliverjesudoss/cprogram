b7eb0d07be3e64fe4f85e5f536457bffabca83cc
sharonchristoliverjesudoss <35994913+sharonchristoliverjesudoss@users.noreply.github.com>
2018-02-19 15:52:56 +0530

    22program19.02.2018.C

---
+++ 22program19.02.2018.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int large(int arr[], int n)
+{
+    int s;
+    int max = arr[0];  
+    for (s = 1; s < n; s++)
+    int arr[] = {10, 324, 45, 90, 9808};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    printf("Large in given array is %d", large(arr, n));
+    return 0;
+}
