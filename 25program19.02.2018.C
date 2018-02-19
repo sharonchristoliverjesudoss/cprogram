a52f6d6996f77251ecd1a0485d99c47820dd90bd
sharonchristoliverjesudoss <35994913+sharonchristoliverjesudoss@users.noreply.github.com>
2018-02-19 16:40:38 +0530

    25program19.02.2018.C

---
+++ 25program19.02.2018.C
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>#include <stdio.h>
+
+int main(void)
+{
+  int n,temp,i,j,a[30];
+  scanf("%d",&n);
+  for(i=0;i<n;i++)
+  {
+  	scanf("%d ",&a[i]);
+  }
+  for(i=0;i<n;i++)
+  {
+  	for(j=0;j<n-1;j++)
+  	{
+  		if(a[j]>a[j+1])
+  		{
+  			temp=a[j];
+  			a[j]=a[j+1];
+  			a[j+1]=temp;
+  		}
+  	}
+  }
+  n=((n+1)/2)-1;
+  printf("%d",a[n]);
+	return 0;
+}
