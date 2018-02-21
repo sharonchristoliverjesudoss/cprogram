4fc55e18b814cc9469375fb3cd9ff1c2e5331271
sharonchristoliverjesudoss <35994913+sharonchristoliverjesudoss@users.noreply.github.com>
2018-02-21 16:40:56 +0530

    Update 29program21.02.2018.C

--- 29program21.02.2018.C
+++ 29program21.02.2018.C
@@ -3,11 +3,9 @@ int main()
 {
       int hrs, min,total_min;
       printf("\nEnter the Time in Hrs and Min:\n");
-      printf("\nHrs:\t");
-      scanf("%d", &hrs);
-      printf("\nMin:\t");
-      scanf("%d", &minutes);
-       total_min = (hrs * 60) + min;
+      printf
+      scanf("%d", &hrs);canf("%d", &minutes);
+
       printf("\nTotal Time in Min:\t%d\n", total_min);
       return 0;
 }
