a4d66df54417d22972e19481967722093463e6a1
sharonchristoliverjesudoss <35994913+sharonchristoliverjesudoss@users.noreply.github.com>
2018-02-26 16:11:44 +0530

    35program26.02.2018.C

---
+++ 35program26.02.2018.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main(void) 
+{
+	char str[50];
+	int j,count=0;
+	printf("enter the sentence:");
+	scanf("%[^\n]s",str);
+	for(j=0;str[j]!='\0';j++)
+	{
+		if(str[j]>='0' && str[j]<='9')
+		{
+			count=count+1;
+		}
+	}
+	printf("\nthe number of numeric in the sentence is:%d",count);
+	return 0;
+}
