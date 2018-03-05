8752b0b5003975947e3579a394803a4eeca2900d
sharonchristoliverjesudoss <35994913+sharonchristoliverjesudoss@users.noreply.github.com>
2018-03-05 15:49:06 +0530

    37program05.02.2018.C

---
+++ 37program05.02.2018.C
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <conio.h>
+int main(void) 
+{
+int a,s;
+printf("\n enter two numbers to swap:");
+scanf("%d %d",&a,&s);
+printf("\n before swapping:");
+printf("\nx=%d",a);
+printf("\ny=%d",s);
+A=a+s;
+s=A-s;
+printf("\n after swapping:");
+printf("\nx=%d",a;
+printf("\ny=%d",s;
+getch();
+return 0;
+}
+ 
