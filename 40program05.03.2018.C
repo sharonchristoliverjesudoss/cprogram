a585d05507df0563de62136b150e33869ee28392
sharonchristoliverjesudoss <35994913+sharonchristoliverjesudoss@users.noreply.github.com>
2018-03-05 16:03:37 +0530

    40program05.03.2018.C

---
+++ 40program05.03.2018.C
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <conio.h>
+int main(void) 
+{
+int q=1,w=1,j,m,d;
+printf("\n enter the range to print the Fibonacci numbers\n");
+scanf("%d",&m);
+printf("%d",q);
+printf("\t%d",b);
+for(j=1;j<=n-2;j++)
+{
+c=a+b;
+printf("\t%d",c);
+a=b;
+b=c;
+}
+getch();
+return 0;
+}                                                                                                                                                                                                                                                                 
