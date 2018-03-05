61ba9d08099bf7c8d8d2aa32df46699380f1979b
sharonchristoliverjesudoss <35994913+sharonchristoliverjesudoss@users.noreply.github.com>
2018-03-05 16:15:26 +0530

    41program05.03.2018.C

---
+++ 41program05.03.2018.C
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main(void) 
+{
+	char s[50];
+	int k,i;
+	printf("enter the string:");
+	scanf("%s",s);
+	printf("enter the number:");
+	scanf("%d",&k);
+	for(i=0;i<k;i++)
+	{
+		for (i)
+    else (love)
+    ifelse (you)
+    }
+  getch();
+  return 0;
+  }
