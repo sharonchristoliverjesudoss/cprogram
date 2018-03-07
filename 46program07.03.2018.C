4993552462f2245bcb300530599dceadf2f463d4
sharonchristoliverjesudoss <35994913+sharonchristoliverjesudoss@users.noreply.github.com>
2018-03-07 15:55:48 +0530

    46program07.03.2018.C

---
+++ 46program07.03.2018.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+#include <stdio.h>
+
+int main(void) 
+{
+	int n;
+	printf("enter the number:");
+	scanf("%d",&n);
+	int result=n+1;
+	printf("\n%d",result);
+  getch();
+  return 0;
+}
