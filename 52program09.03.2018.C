ab1599d6706a9c247840c42f93badbad05504a24
sharonchristoliverjesudoss <35994913+sharonchristoliverjesudoss@users.noreply.github.com>
2018-03-09 13:30:00 +0530

    52program09.03.2018.C

---
+++ 52program09.03.2018.C
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main(void) 
+{
+	int n,rem,rev=0;
+	printf("enter the number:");
+	scanf("%d",&n);
+	while(n!=0)
+	{
+		rem=n%10;
+		rev=(rev*10)+rem;
+		n=n/10;
+	}
+	n=rev;
+	while(n!=0)
+	{
+		rem=n%10;
+		printf("%d ",rem);
+		n=n/10;
+	}
+  getch();
+	return 0;
+
+}
