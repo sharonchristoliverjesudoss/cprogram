f46b8a5eac68f95b3b2ed0cb0e751ccd6820f677
sharonchristoliverjesudoss <35994913+sharonchristoliverjesudoss@users.noreply.github.com>
2018-03-09 13:14:29 +0530

    49program07.03.2018.C

---
+++ 49program07.03.2018.C
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main(void) 
+{
+	int m1,m2,sum;
+	printf("\n enter the first number:");
+	scanf("%d",&m1);
+	printf("\n enter the second number:");
+	scanf("%d",&m2);
+	sum=m1+m2;
+	printf("%d",sum);
+  getch();
+	return 0;
+}
