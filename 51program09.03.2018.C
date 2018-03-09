e22152dac473c8566d48419480be422a70b12b0c
sharonchristoliverjesudoss <35994913+sharonchristoliverjesudoss@users.noreply.github.com>
2018-03-09 13:38:36 +0530

    51program09.03.2018.C

--- 51program09.03.2018.C
+++ 51program09.03.2018.C
@@ -3,21 +3,21 @@
 
 int main(void) 
 {
-	int n,rem,rev=0;
+	int m,rem,rev=0;
 	printf("enter the number:");
-	scanf("%d",&n);
-	while(n!=0)
+	scanf("%d",&m);
+	while(m!=0)
 	{
 		rem=n%10;
 		rev=(rev*10)+rem;
 		n=n/10;
 	}
-	n=rev;
-	while(n!=0)
+	m=rev;
+	while(m!=0)
 	{
-		rem=n%10;
+		rem=m%10;
 		printf("%d ",rem);
-		n=n/10;
+		m=m/10;
 	}
   getch();
 	return 0;
