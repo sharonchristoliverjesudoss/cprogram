d1ff039d254949890f975c013708cc5df41849e9
sharonchristoliverjesudoss <35994913+sharonchristoliverjesudoss@users.noreply.github.com>
2018-03-09 13:10:55 +0530

    48program09.03.2018.C

---
+++ 48program09.03.2018.C
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main(void) 
+{
+	int j,m,array[20],sum=0,avg;
+	printf("enter the size of the array");
+	scanf("%d",&m);
+	printf("enter the array elements");
+	for(j=0;j<n;j++)
+	{
+		scanf("%d",&array[j]);
+	
+	}
+	for(j=0;j<n;j++)
+	{
+		sum=sum+array[j];
+	}
+	avg=sum/n;
+	printf("\n%d",sum);
+	printf("\n%d",avg);
+  getch();
+	return 0;
+}
