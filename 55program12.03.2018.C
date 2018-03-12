630353a3aa1a1dcd14b04f22974e69a4c15e0ed4
sharonchristoliverjesudoss <35994913+sharonchristoliverjesudoss@users.noreply.github.com>
2018-03-12 15:50:16 +0530

    55program12.03.2018.C

---
+++ 55program12.03.2018.C
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <conio.h>
+int main(void) 
+{
+	int i,j,pro;
+	printf("enter the number1:");
+	scanf("%d",&i);
+	printf("\nenter the number2:");
+	scanf("%d",&j);
+	pro=n*m;
+	if(pro%2==0)
+	{
+		printf("\n even");
+	}
+	else
+	{
+		printf("\n odd");
+	}
+	return 0;
+}
+ 
