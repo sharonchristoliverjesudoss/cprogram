170f5f07317c4aee9feaf3292a4e094b2c86c9d5
sharonchristoliverjesudoss <35994913+sharonchristoliverjesudoss@users.noreply.github.com>
2018-03-14 16:00:59 +0530

    58program13.03.2018.C

---
+++ 58program13.03.2018.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(void) 
+{
+int i,y,temp;
+printf("\n enter two numbers to swap:");
+scanf("%d %d",&i,&y);
+printf("\n before swapping:");
+printf("\nx=%d",8i);
+printf("\ny=%d",y);
+temp=i;
+i=y;
+y=temp;
+printf("\n after swapping:");
+printf("\nx=%d",x);
+printf("\ny=%d",y);
+return 0;
+}
