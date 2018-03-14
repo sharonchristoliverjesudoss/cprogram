89e3e12e2788157fd299f61730082c1e442a47d6
sharonchristoliverjesudoss <35994913+sharonchristoliverjesudoss@users.noreply.github.com>
2018-03-14 16:04:43 +0530

    59 program13.03.2018.C

---
+++ 59 program13.03.2018.C
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <conio.h>
+
+
+
+int main(void) 
+
+{
+
+	int array[20];
+
+	int n,i,max=array[0];
+
+	printf("enter the number of elements:");
+
+	scanf(intf("\n enter the array elements:");
+
+0;i<n;i++)
+
+	{
+
+		scanf("%d",&array[i]);
+
+	}
+
+	for(i=0;i<n;i++)
+
+	{
+
+		i[i]>max)
+
+		{
+
+			max=array[i];
+
+		}
+
+	}
+
+	printf("\nthe maximum of the numbers is:%d",max);
+
+	return 0;
+
+}
