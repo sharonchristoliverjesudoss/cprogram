82074506932f1a01399fa4fead962336f2768e86
sharonchristoliverjesudoss <35994913+sharonchristoliverjesudoss@users.noreply.github.com>
2018-03-14 16:09:22 +0530

    60program13.03.2018.C

---
+++ 60program13.03.2018.C
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include
+<conio.h>
+
+
+
+int main(void) 
+
+{
+
+int a=1,b=1,i,n,c;
+
+printf("\n enter the range to print the Fibonacci numbers\n");
+
+scanf("%d",&n);
+
+printf("%d",a);
+
+c=a+b;
+
+printf("\t%d"
