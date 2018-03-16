7d4c6a35ba932c8c94418fa1ddcac14967f5e7f3
sharonchristoliverjesudoss <35994913+sharonchristoliverjesudoss@users.noreply.github.com>
2018-03-16 16:16:46 +0530

    65program16.03.2018.C

---
+++ 65program16.03.2018.C
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void) 
+{
+	char str[50];
+	int i;
+	printf("enter the string:");
+	scanf("%[^\n]s",str);
+	if(printf("\n%s",str))
+	{
+		
+	}
+	return 0;
+}
